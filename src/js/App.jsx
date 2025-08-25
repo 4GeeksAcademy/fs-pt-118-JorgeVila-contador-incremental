a4971584f8b0f8575eb4b43dd7247cacfdb39c9b
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2025-08-25 18:34:47 +0000

    feat: modificacion plantilla base

---
+++ src/js/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contador from "./components/Contador";
+//create your first component
+const App = () => {
+	return (
+		<div >
+			<h1>HOLIIIIII</h1>
+			<Contador />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/js/components/Contador.jsx
@@ -0,0 +1,17 @@
+
+//create your first component
+const Contador = () => {
+    return (
+        <div >
+            <span>0</span>
+            <span>0</span>
+            <span>0</span>
+            <span>0</span>
+            <span>0</span>
+            <span>0</span>
+
+        </div>
+    );
+};
+
+export default Contador;
